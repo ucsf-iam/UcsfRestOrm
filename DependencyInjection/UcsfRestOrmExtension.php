7a6cb6d6cd0009b49465921e4c2fd20e1cac5dda
Jason Gabler <jasongabler@gmail.com>
2016-10-07 16:43:33 -0700

    Fixing config processing for multiple connections, entity managers, etc.

--- DependencyInjection/UcsfRestOrmExtension.php
+++ DependencyInjection/UcsfRestOrmExtension.php
@@ -20,23 +20,22 @@ class UcsfRestOrmExtension extends Extension
     /**
      * {@inheritdoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $entityManagerAliasTemplate = 'ucsf_rest_orm.%s_entity_manager';
-
-        $configuration = new Configuration();
+    public function load(array $configs, ContainerBuilder $container) {
+        $configuration = $this->getConfiguration($configs, $container);
         $config = $this->processConfiguration($configuration, $configs);
+
+        $entityManagerAliasTemplate = 'ucsf_rest_orm.%s';
+        $container->setParameter('ucsf_rest_orm_config', $config);
         $entityManagers = $config['entity_managers'];
         $connections = $config['connections'];
 
-        $container->setParameter('ucsf_rest_orm_config', $config);
         foreach ($entityManagers as $entityManagerName => $entityManagerConfig) {
             $entityManagerAlias = sprintf($entityManagerAliasTemplate, $entityManagerName);
             $container->setAlias($entityManagerAlias, 'ucsf_rest_orm_entity_manager');
             $container
                 ->setDefinition($entityManagerAlias, new Definition(EntityManager::class))
                 ->setArguments([
-                    $connections[$entityManagerName],
+                    $connections[$entityManagerConfig['connection']],
                     empty($entityManagerConfig['repositories']) ? [] : $entityManagerConfig['repositories'],
                     empty($entityManagerConfig['commands']) ? [] : $entityManagerConfig['commands']
                 ]);
